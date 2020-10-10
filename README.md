7e6aee7e431d0669b3d5322f4370140c79592b21
mittsahl <msalem311@aol.com>
2020-10-10 21:34:46 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+This c file will format betty compliant files for you
+How to use:
+
+1. Get a copy of the format.c file to current working directory
+
+2. Compile to format.c file using "gcc format.c -o format"
+
+3.  Use it like this:
+	./format [Name Of File] "[Function Return Type and Name]" "[Variable/s for function]"
+
+4.  Then you should have your file name in current working directory with Betty Compliant format!
+
+5.  Let me know if you encounter any bugs or issues and let me know on slack: Matthew Salem
+
+
---
+++ format.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char **argv)
+{
+	FILE *fp;
+	char *brackets = "{\n\n}\n\n";
+	char *headers = "#include <stdio.h>\n#include \"holberton.h\"\n\n/**\n";
+	char *ptr;
+	int i, length = 0;
+	char *copy;
+	
+	fp = fopen(argv[1], "w");
+	fputs(headers, fp);
+	for (i =  2; i < argc; i++)
+	{
+		fputs("* ",fp);
+		if (i > 2)
+			fputc('@', fp);
+		ptr = argv[i];
+		while (*ptr != ' ')
+			ptr++;
+		while (isalpha(*ptr) == 0)
+			ptr++;
+		copy = ptr;
+		while (*copy != '\0')
+		{	
+			copy++;
+			length++;
+		}
+		copy = malloc(length);
+		strcpy(copy, ptr);
+		fputs(copy, fp);
+		if (i == 2)
+			fputs(" - ", fp);		
+		else
+			fputs(": ", fp);
+		fputs(" \n", fp); 
+	}	
+	fputs("* Return: \n*/\n\n", fp);
+	for (i = 2; i < argc; i++)
+	{
+		fputs(argv[i], fp);	
+		if (i == 2)
+			fputc('(', fp);
+		else if (i == argc - 1)
+			fputs(")\n", fp);
+		else
+			fputs(", ", fp);
+	}
+	fputs(brackets, fp);
+	fclose(fp);
+	return (0);
+}
